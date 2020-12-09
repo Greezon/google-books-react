fe65ae42383e58298e31f2f8da1403f93cee1d91
Ian A <iastesana@gmail.com>
2020-12-08 17:29:12 -0800

    added pages, jumbo tron, nav bar, and basic MVC

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Google Books React App.</h1>
+      <a target="_blank" rel="noopener noreferrer" href="google.com">
+        Powered By yours truly Ian N Astesana
+      </a>
+    </div>
+  );
+}
+
+export default Jumbotron;
